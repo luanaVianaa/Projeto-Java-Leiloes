02a1eeb360de4759dc04891e5ecc3e080e94905e
Luana Viana <vianaluana.souza@gmail.com>
2024-07-25 16:46:17 -0300

    Criado a funcionalidade de Salvar os dados inseridos no banco de dados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -12,6 +12,7 @@ import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 
@@ -21,20 +22,44 @@ public class ProdutosDAO {
     PreparedStatement prep;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+  
     
     public void cadastrarProduto (ProdutosDTO produto){
         
         
-        //conn = new conectaDAO().connectDB();
+        conn = new conectaDAO().connectDB();
         
         
     }
     
     public ArrayList<ProdutosDTO> listarProdutos(){
         
+        String sqlFiltro = "SELECT * FROM produtos";
+        
         return listagem;
+        
+   
     }
+
     
+     public int salvar(ProdutosDTO produtos){
+        int status;
+        try {
+            prep = conn.prepareStatement("INSERT INTO produtos (nome, valor, status) VALUES(?,?,?)");
+
+            prep.setString(1,produtos.getNome());
+            prep.setInt(2,produtos.getValor());
+            prep.setString(3,produtos.getStatus()); 
+
+
+          
+            status = prep.executeUpdate();
+            return status; 
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+            return ex.getErrorCode();
+        }
+    }
     
     
         
